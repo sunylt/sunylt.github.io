cdeb27b45e4c911df970078ca30e7b41d4e6f9dc
Tony <10666779@qq.com>
2019-04-17 14:59:21 +0800

    fx
    fx

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66f5361c4721e3cb8aa28b82d4c66050.js"
+  "/precache-manifest.e1d34de1d319671ee743b2ea8b134ee6.js"
 );
 
 workbox.clientsClaim();
