40f1039daa03b13bc26fdbc6a0a8563c6c543617
刘涛 <10666779@qq.com>
2022-04-19 16:00:09 +0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -107,9 +107,7 @@ const service = window.service = new emedia.Service({
 			const videoTrack = mediaStream.getVideoTracks()[0]
 			const customMediaStream = new MediaStream()
 			customMediaStream.addTrack(videoTrack)
-			console.warn("+++++++++++++++", mediaStream)
-			// console.warn("+++++++++++++++", mediaStream.getVideoTracks())
-			console.log(videoTrack)
+			
 			// 针对桌面共享单独处理
 			if(stream.type == 1){
 				$("#" + stream.id + " video").srcObject = mediaStream
