1458e667c1314aaf0c14791aeeff8698445df1ec
刘涛 <10666779@qq.com>
2022-04-20 16:10:25 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -207,7 +207,7 @@ function joinRoom(roomId) {
 
 		// 加入房间然后打开设备并推流
 		service.join(() => {
-			publishMediaStream({ audio: true, video: true }) // 流配置
+			// publishMediaStream({ audio: true, video: true }) // 流配置
 		}, () => alert('Join room error.'))
 	})
 }
